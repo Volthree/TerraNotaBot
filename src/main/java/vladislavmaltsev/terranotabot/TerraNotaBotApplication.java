d40b38a95eb150937560f39ca196da42070a3081
Vladislav Maltsev <Vladiysvelikolepniy@gmail.com>
2024-01-24 12:52:16 +0300

    Heroku 2

--- src/main/java/vladislavmaltsev/terranotabot/TerraNotaBotApplication.java
+++ src/main/java/vladislavmaltsev/terranotabot/TerraNotaBotApplication.java
@@ -3,6 +3,7 @@ package vladislavmaltsev.terranotabot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
--- src/main/java/vladislavmaltsev/terranotabot/repository/UserParametersRepository.java
+++ src/main/java/vladislavmaltsev/terranotabot/repository/UserParametersRepository.java
@@ -3,10 +3,8 @@ package vladislavmaltsev.terranotabot.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import vladislavmaltsev.terranotabot.enity.UserParameters;
-
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 public interface UserParametersRepository extends JpaRepository<UserParameters, Integer> {
